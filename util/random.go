b6ff5422173b38f78a57b96252fa3831190c5c84
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-08-18 08:02:26 -0300

    feat: add user_test

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -40,4 +41,8 @@ func RandomCurrency() string {
 	currencies := []string{USD, EUR, BRL}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
